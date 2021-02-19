c6a52b957350ee22b0e1320bb1beac2525d2fa58
Kamil Kula <kamil11kula@gmail.com>
2021-02-19 15:44:24 +0100

    Done

--- TMCProjects/mooc-java-programming-ii/part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
+++ TMCProjects/mooc-java-programming-ii/part08-Part08_08.PrintMeMyHashmap/src/main/java/Program.java
@@ -4,7 +4,40 @@ import java.util.HashMap;
 public class Program {
 
     public static void main(String[] args) {
-        // Test your program here!
+
+        HashMap<String, String> hashmap = new HashMap<>();
+        hashmap.put("f.e", "for example");
+        hashmap.put("etc.", "and so on");
+        hashmap.put("i.e", "more precisely");
+
+        printKeys(hashmap);
+        System.out.println("---");
+        printKeysWhere(hashmap, "i");
+        System.out.println("---");
+        printValuesOfKeysWhere(hashmap, ".e");
+
+    }
+
+    public static void printKeys(HashMap<String,String> hashmap) {
+        for (String hash : hashmap.keySet()) {
+            System.out.println(hash);
+        }
+    }
+
+    public static void printKeysWhere(HashMap<String,String> hashmap, String text) {
+        for (String hash : hashmap.keySet()) {
+            if (hash.contains(text)) {
+                System.out.println(hash);
+            }
+        }
+    }
+
+    public static void printValuesOfKeysWhere(HashMap<String,String> hashmap, String text) {
+        for (String hash : hashmap.keySet()) {
+            if (hash.contains(text)) {
+                System.out.println(hashmap.get(hash));
+            }
+        }
     }
 
 }
