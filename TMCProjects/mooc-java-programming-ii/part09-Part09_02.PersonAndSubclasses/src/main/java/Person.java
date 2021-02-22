d7b8ddee0c5385d8553f497a5cda4c24feaa4c24
Kamil Kula <kamil11kula@gmail.com>
2021-02-22 15:54:50 +0100

    Done

---
+++ TMCProjects/mooc-java-programming-ii/part09-Part09_02.PersonAndSubclasses/src/main/java/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+
+    private final String name;
+    private final String address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + "\n  " + this.address;
+    }
+}
