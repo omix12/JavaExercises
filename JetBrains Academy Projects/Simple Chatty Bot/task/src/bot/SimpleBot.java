734e6eed973f0dda9c9821182bef3142362d6704
Kamil Kula <kamil11kula@gmail.com>
2021-03-08 15:02:32 +0100

    Simple Chatty Bot project added

---
+++ JetBrains Academy Projects/Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -0,0 +1,70 @@
+package bot;
+
+import java.util.Scanner;
+
+public class SimpleBot {
+    final static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        greet();
+        remindName();
+        guessAge();
+        count();
+        test();
+        end();
+    }
+
+    static void greet() {
+        System.out.println("Hello! My name is " + "Aid" + ".");
+        System.out.println("I was created in " + "2018" + ".");
+        System.out.println("Please, remind me your name.");
+    }
+
+    static void remindName() {
+        String name = scanner.nextLine();
+        System.out.println("What a great name you have, " + name + "!");
+    }
+
+    static void guessAge() {
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+    }
+
+    static void count() {
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+        int num = scanner.nextInt();
+        for (int i = 0; i <= num; i++) {
+            System.out.printf("%d!\n", i);
+        }
+    }
+
+    static void test() {
+        System.out.println("Let's test your programming knowledge.");
+
+        System.out.println("""
+                1. To repeat a statement multiple times.
+                2. To decompose a program into several small subroutines.
+                3. To determine the execution time of a program.
+                4. To interrupt the execution of a program.""");
+
+        while (true) {
+
+            if (scanner.nextInt() != 2) {
+                System.out.println("Please, try again.");
+            } else {
+                System.out.println("Congratulations, have a nice day!");
+                break;
+            }
+
+        }
+    }
+
+    static void end() {
+        System.out.println("Congratulations, have a nice day!");
+    }
+}
